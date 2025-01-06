2f1276bbf6f330efca7f155e62df625ce2e1bef3
DakeshnaSiva <dakeshnasiva8939@gmail.com>
2025-01-06 17:20:52 +0530

    error handling methods

---
+++ src/exception.py
@@ -0,0 +1,26 @@
+
+import sys
+import logging
+
+def error_message_details(error,error_details:sys):
+    _,_,tb=error_details.exc_info()
+    file_name = tb.tb_frame.f_code.co_filename
+    line_number = tb.tb_lineno
+    error_message = f"file name: {file_name}, line number: {line_number}, error: {error}"
+    return error_message
+
+class CustomException(Exception):
+    def __init__(self,error_message,error_details:sys):
+       super().__init__(error_message)
+       self.error_message_details = error_message_details(error_message,error_details=error_details)
+
+    def __str__(self):
+        return self.error_message_details
+    
+# if __name__ == "__main__":
+#     try:
+#        a=1/0
+#     except Exception as e:
+#         logging.info("Not Divisible by zero")
+#         raise CustomException(e,sys)
+

0fe721a3024e86d86e4d2736f0a9b1f720ba948b
DakeshnaSiva <dakeshnasiva8939@gmail.com>
2025-01-06 15:33:26 +0530

    new push

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ def get_requirements(file_path: str) -> List[str]:
 
 setup(
     name="my_package",
-    version="0.0.1",
+    version="0.0.18",
     author="Dakeshna",
     packages=find_packages(),
     install_requires=get_requirements("requirements.txt"),
